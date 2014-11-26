adda38a0cf886d1de0483608afdffe4e1d910459
classmateyang <classmateyang@gmail.com>
2014-11-25 23:19:05 -0500

    add some comments

--- plot/testchap5.py
+++ plot/testchap5.py
@@ -1,35 +1,32 @@
 __author__='yangyang'
 import numpy as np
 
+def f(x):
+	return 2
 
-#using np.where to vectorize
-def Nv(x):
-	cond1=x < 0
-	cond2=np.logical_and(0<=x,x<1)
-	cond3=np.logical_and(1<=x,x<2)
-	cond4=x >= 2
-	r=np.zeros(len(x))
-	r=np.where(cond1, 0., r)
-	r=np.where(cond2, x, r)
-	r=np.where(cond3, 2-x, r)
-	r=np.where(cond4, 0., r)
-	return r
-#using boolen indexing to vectorize
+def fv(x):
+	if isinstance(x, (float, int)):
+		return 2
+	elif isinstance(x, np.ndarray):
+		return np.zeros(x.shape, x.dtype)+2
+	else:
+		raise TypeError\
+			('x must be int, float or ndarray, not %s'%type(x))
 
+x=np.linspace(1,10,50)
 
-def Nv2(x):
-	cond1=x < 0
-	cond2=np.logical_and(0 <= x, x < 1)
-	cond3=np.logical_and(1 <= x, x < 2)
-	cond4=x >= 2
-	r=np.zeros(len(x))
-	r[cond1] =0.
-	r[cond2] =x[cond2]
-	r[cond3] =2-x[cond3]
-	r[cond4] =0.
-	return r
+t=np.linspace(1,30,30).reshape(5,6)
 
-x=np.linspace(-10,10,40)
+print t
+print '\n'
+print np.ix_([2,4]),np.ix_([3,4])
+print '\n'
+print t[np.ix_([2,4]),np.ix_([3,4])]
+print '\n'
+print t[np.ix_([2,4])]
+print '\n'
+print t[np.ix_([3,4])]
+print '\n'
 
-print Nv(x)
-print Nv2(x)
+print t[:,np.ix_([3,4])]
+print '\n'
